002f1683dfed7d21c391874e85b379600a771e95
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2022-11-24 18:48:33 +0100

    24 November first half

--- 5-array.js
+++ 5-array.js
@@ -1,17 +1,60 @@
-let array_num=[
-    1,2,3,4,5
+let array_num = [
+    1, 2, 3, 4, 5
 ]
-let array_obj=[
-    {name:"John"},
-    {name:"John 1"},
-    {name:"John 2"}
+let array_obj = [{
+        name: "John",
+        age: 15
+    },
+    {
+        name: "John 1",
+        age: 20
+    },
+    {
+        name: "John 2",
+        age: 10
+    }
 ]
-array_num.forEach((el)=>{console.log(el)})
+array_num.forEach((el) => {
+    console.log(el)
+})
 
-let multipliedNums = array_num.map((el)=>{
-    return el*el
+let multipliedNums = array_num.map((el) => {
+    return el * el
 })
 
 console.log(multipliedNums);
 
 array_obj.forEach((obj)=>{console.log(obj.name)})
+
+//! ------filter
+
+array_obj.filter((obj)=>{ if(obj.age>18) console.log(obj);})
+
+const filterPerson = array_obj.filter((obj) => {
+    if (obj.age > 18) return obj
+})
+
+console.log(filterPerson);
+
+
+//! ------reduce
+
+let sum = array_num.reduce((acc, curr)=>{
+    return acc+curr
+},5)
+
+console.log(sum);
+
+let spending = [
+    {item:"Grocery", price:25},
+    {item:"Shopping", price:125},
+    {item:"Mobile", price:225},
+    {item:"Movie", price:325},
+]
+
+
+let total = spending.reduce((acc,curr)=>{
+    return acc+curr.price
+},0)
+
+console.log(total);
---
+++ 6-primvsRef.js
@@ -0,0 +1,19 @@
+let num1 = 10
+let num2 = num1
+
+num1 =20
+
+console.log(num1);
+console.log(num2);
+
+
+let person ={
+    name:"John",
+    age:20
+};
+
+let person2 = person;
+person2.age =30;
+
+console.log(person);
+console.log(person2);
